4a376bec74a062c460d5e48b103a4800cefbc317
Borko Djurovic <borko.djurovic@hotmail.com>
2025-02-18 08:58:47 +0100

    Add project files.

---
+++ ASP_Web_MVC_App/Program.cs
@@ -0,0 +1,66 @@
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Azure;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Azure.Core;
+
+namespace ASP_Web_MVC_App
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var keyVaultEndpoint = new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/");
+            builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+
+            // Add services to the container.
+            builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(options =>
+                {
+                    options.ClientId = builder.Configuration["ClientIdWebApp"];
+                    options.TenantId = builder.Configuration["TenantIdWebApp"];
+                    options.Domain = "sdkpractice-cfffd2ewbagqagcy.canadacentral-01.azurewebsites.net";
+                    options.Instance = "https://login.microsoftonline.com/";
+                    options.CallbackPath = "/signin-oidc";
+
+                });
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
